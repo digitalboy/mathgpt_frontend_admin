d3c216e8b1b959ecf3b9112f899bc67f60f14620
David Zhang <digitalboyzone@gmail.com>
2024-04-25 03:03:40 +0800

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),
   ],
-  base: "./",
+  base: "admin/",
   resolve: {
     alias: {
       '@': '/src', // 设置 '@' 为 'src' 目录的别名
